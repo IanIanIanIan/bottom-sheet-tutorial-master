02edc35b0199eaa4fecbb15657ec92e1ea150a77
gengyanbin <gengyanbin@jd.com>
2020-04-03 22:14:45 +0800

    bottom sheet behavior version two

--- app/src/main/java/com/androidtutorialshub/bottomsheets/NestedScrollViewBehavior.java
+++ app/src/main/java/com/androidtutorialshub/bottomsheets/NestedScrollViewBehavior.java
@@ -1,21 +1,18 @@
 package com.androidtutorialshub.bottomsheets;
 
-import android.app.Activity;
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.NestedScrollView;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 
-import java.util.List;
 
-
-public class NestedScrollViewBehavior extends BaseHeaderScrollingViewBehavior {
+public class NestedScrollViewBehavior extends AppBarLayout.ScrollingViewBehavior {
 
     private BottomSheetBehavior bottomSheetBehavior;
 
@@ -29,28 +26,8 @@ public class NestedScrollViewBehavior extends BaseHeaderScrollingViewBehavior {
         super(context, attrs);
     }
 
-    @Override
-    public View findFirstDependency(List<View> views) {
-        if (views != null) {
-            for (int i = 0; i < views.size(); i++) {
-                if (isDependency(views.get(i))) {
-                    return views.get(i);
-                }
-            }
-        }
-        return null;
-    }
 
-    /**
-     * 是否依赖
-     *
-     * @param dependency dependency
-     * @return 是否依赖
-     */
-    private boolean isDependency(View dependency) {
-        return dependency != null && dependency.isShown()
-                && (dependency.getId() == R.id.bottom_sheet);
-    }
+
 
     /**
      * 是否正在滑动
@@ -70,7 +47,7 @@ public class NestedScrollViewBehavior extends BaseHeaderScrollingViewBehavior {
     private int mTouchSlop = -1;
 
     @Override
-    public boolean onInterceptTouchEvent(CoordinatorLayout parent, NestedScrollView child, MotionEvent ev) {
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, View child, MotionEvent ev) {
         //手指上滑
         getbottomsheet(parent);
         if (mTouchSlop < 0) {
@@ -99,9 +76,9 @@ public class NestedScrollViewBehavior extends BaseHeaderScrollingViewBehavior {
             default:
                 break;
         }
-
         return super.onInterceptTouchEvent(parent,child,ev);
     }
+
 
 
     private void getbottomsheet(CoordinatorLayout parent) {
@@ -125,82 +102,6 @@ public class NestedScrollViewBehavior extends BaseHeaderScrollingViewBehavior {
 
         }
     }
-
-    /*@Override
-    public boolean onInterceptTouchEvent(CoordinatorLayout parent, RecyclerView child, MotionEvent ev) {
-
-        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            isFromRecyclerViewDown = ev.getY() >= child.getY();
-        }
-        if (ev.getY() >= child.getY() && isFromRecyclerViewDown) {
-            if (mIsBeingDragged) {
-                mIsBeingDragged = false;
-                if (canDragView(child) && parent.getContext() instanceof Activity) {
-                    //ProductListHeaderHelper.handleHeaderViewsStatus((Activity) parent.getContext());
-                }
-            }
-        }
-        if (mTouchSlop < 0) {
-            mTouchSlop = ViewConfiguration.get(parent.getContext()).getScaledTouchSlop();
-        }
-        //处理头部事件
-        if (!isFromRecyclerViewDown) {
-            switch (ev.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN: {
-                    mIsBeingDragged = false;
-                    final int x = (int) ev.getX();
-                    final int y = (int) ev.getY();
-                    mLastMotionDownX = x;
-                    mLastMotionY = y;
-                    if (canDragView(child)) {
-                        ev.setLocation(ev.getX(), ev.getY() - child.getY());
-                        child.onTouchEvent(ev);
-                        ev.setLocation(x, y);
-                    }
-                    break;
-                }
-                case MotionEvent.ACTION_MOVE: {
-                    final int y = (int) ev.getY();
-                    final int yDiff = Math.abs(y - mLastMotionY);
-
-                    if (yDiff > mTouchSlop) {
-                        mIsBeingDragged = true;
-                        mLastMotionY = y;
-                    }
-                    if (mIsBeingDragged && canDragView(child)) {
-                        ev.setLocation(ev.getX(), ev.getY() - child.getY());
-                        child.onTouchEvent(ev);
-                        // 缓解 触发 品牌平铺横向滑动 问题
-                        ev.setLocation(mLastMotionDownX, y);
-                        if (mIsBeingDragged) {
-                            return false;
-                        }
-                    }
-                    break;
-                }
-                case MotionEvent.ACTION_CANCEL:
-                case MotionEvent.ACTION_UP: {
-                    if (mIsBeingDragged) {
-                        mIsBeingDragged = false;
-                        if (canDragView(child) && parent.getContext() instanceof Activity) {
-                            //ProductListHeaderHelper.handleHeaderViewsStatus((Activity) parent.getContext());
-                        }
-                        return true;
-                    }
-                    // UP CANCEL 事件也需要recyclerView执行，收尾前面的DOWN事件
-                    final int y = (int) ev.getY();
-                    ev.setLocation(ev.getX(), ev.getY() - child.getY());
-                    child.onTouchEvent(ev);
-                    ev.setLocation(ev.getX(), y);
-                    mIsBeingDragged = false;
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
-        return super.onInterceptTouchEvent(parent, child, ev);
-    }*/
 
 
     /**
